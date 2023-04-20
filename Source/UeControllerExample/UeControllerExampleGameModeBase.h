8c96d312caac133f72b28b43214a99da0398812c
IsArnoAh <2286430915@qq.com>
2023-04-20 09:31:04 +0800

    项目初始化

---
+++ Source/UeControllerExample/UeControllerExampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UeControllerExampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UECONTROLLEREXAMPLE_API AUeControllerExampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
